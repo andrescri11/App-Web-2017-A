0a81f79e0bf93bdddf3f554b783ae4c3dd762667
andrescri11 <USRDEL@USR16.local>
2017-05-22 20:06:47 -0500

    funcionesJavaScript

--- JavaScript/Funciones/functions.js
+++ JavaScript/Funciones/functions.js
@@ -1,3 +1,21 @@
 /**
  * Created by USRDEL on 22/5/17.
  */
+//void - undefined
+function ImprimirHolaMundo() {
+    console.log("Hola Mundo");
+}
+
+ImprimirHolaMundo(); //undefined
+
+function Imprimir(texto) {
+    console.log(texto);
+}
+
+Imprimir("Cristhian"); //undefined
+
+function Suma2Numeros(a,b) {
+    return a+b;
+}
+
+console.log(Suma2Numeros(1,2)); //3
