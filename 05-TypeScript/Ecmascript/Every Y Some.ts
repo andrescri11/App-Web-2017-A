88ee359c0d0f6f7df27b58594bfe8f1d66b8672d
andrescri11 <USRDEL@USR16.local>
2017-05-29 20:55:59 -0500

    javascriptOperadores

--- 05-TypeScript/Ecmascript/Every Y Some.ts
+++ 05-TypeScript/Ecmascript/Every Y Some.ts
@@ -1,9 +1,27 @@
 /**
  * Created by USRDEL on 29/5/17.
  */
-let arreglo = [1,2,3,4,5];
+let arreglo = [0,8,9,10,5,7];
 let resultado = arreglo.map((valor,indice,arreglo)=>{
-    return valor ** indice;
+    return valor + indice;
+}).some((valor)=>{
+    return valor<=2;
 });
 
 console.log(resultado);
+
+//and - todos cumplen verdadero sino falso
+
+ let resultado2 = arreglo.every((
+     valor,indice,arreglo)=>{
+        return (valor >= 2);
+ });
+
+ console.log(resultado2);
+
+let resultado3 = arreglo.some((
+    valor,indice,arreglo)=>{
+    return (valor <= 2);
+});
+
+console.log(resultado3);
