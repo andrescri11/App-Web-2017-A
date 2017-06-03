2e97b828977ba9fe861b5acc7fb6b4e38796beca
andrescri11 <USRDEL@USR16.local>
2017-06-03 07:29:31 -0500

    Find

---
+++ 05-TypeScript/Ecmascript/Find.js
@@ -0,0 +1,20 @@
+/**
+ * Created by USRDEL on 3/6/17.
+ */
+var arreglo = [
+    {
+        nombre: "Cristhian",
+        apellido: "Chulca",
+        id: 1
+    },
+    {
+        nombre: "Andres",
+        apellido: "Azcasivar",
+        id: 2
+    },
+    {
+        nombre: "Alann",
+        apellido: "Chulca",
+        id: 3
+    },
+];
