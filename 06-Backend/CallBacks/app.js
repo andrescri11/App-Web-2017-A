a68c22c3b303726108b12e33fc20e589b17175d5
andrescri11 <USRDEL@USR16.local>
2017-06-05 20:55:25 -0500

    node
    callback
    instalación de paquete

--- 06-Backend/CallBacks/app.js
+++ 06-Backend/CallBacks/app.js
@@ -1,6 +1,45 @@
 const fs = require('fs');
+//viene instalado x defecto en el node
+/*
+const calculadora = require('cristhianandres');
 
+//const Passwords = require('machinepack-passwords');
+
+ let a = 2,
+    b = 3,
+    resultado;
+
+    resultado = calculadora.calculadoraUdla.sumar(a,b);
+
+    console.log(`Resultado es : ${resultado}`);
+
+    calculadora.calculadoraUdla.passwords.encryptedPassword(
+        {
+            password : passwordAEncriptar,
+        })
+        .exec ({
+            error:(error)=>{
+                console.log(`Error : ${error})
+            }
+        }
+    )
+
+
+
+//npm express
+// modulo que gestiona los request y response del servidor
+*/
 console.log("Empieza");
+
+//CALLBACK
+
+
+// 1 nombre y ruta del archivo
+// 2 codificacion
+// 3 anonymous function
+// (error!, contenido del archivo)
+
+
 
 fs.readFile('texto.txt',"utf8",function
     (err, data){
